97a291b04fd2a848732977904fd2d2d5d8fa75d5
Fernando Concatto <fernandoconcatto@gmail.com>
2016-04-14 18:17:01 -0300

    Plano para interface

--- index.js
+++ index.js
@@ -6,6 +6,32 @@ var SerialPort = require("serialport").SerialPort
 var currentValue = 0;
 var port = new SerialPort("/dev/ttyUSB0");
 
+var Arduino = function(serialPort) {
+  this.serialPort = serialPort;
+}
+
+/**
+ * Value: 0 to 1, where 1 is HIGH.
+ */
+Arduino.prototype.write = function(command, value) {
+  if (this.serialPort.isOpen()) {
+    console.log("writing " + command + ", " + value);
+    this.serialPort.write(new Buffer([command, value]));
+  } else {
+    console.log("not open");
+  }
+}
+
+Arduino.prototype.control = function(pin, value) {
+  this.write(pin, Math.round(value * 255));
+}
+
+Arduino.prototype.wait = function(value) {
+  this.write(1, Math.round(value / 5.0));
+}
+
+var arduino = new Arduino(port);
+
 express.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html");
 });
@@ -16,14 +42,27 @@ function broadcastValue(socket) {
 }
 
 io.on("connection", function(socket) {
-  broadcastValue(socket);
+  socket.on("code", function(data) {
+    var commands = data.split("\n");
+    for (var i = 0; i < commands.length; i++) {
+      var tokens = commands[i].split(" ");
+      console.log(tokens);
+      if (tokens[0] == "wait") {
+        arduino.wait(parseInt(tokens[1]));
+      } else if (tokens[0] == "control") {
+        arduino.control(parseInt(tokens[1]), parseInt(tokens[2]));
+      }
+    }
+  });
+
+  /*broadcastValue(socket);
   socket.on("brightness", function(value) {
     currentValue = Math.pow(1 + (parseInt(value)/47.741), 3) - 1;
     broadcastValue(socket);
     if (port.isOpen()) {
       port.write(new Buffer([currentValue]));
     }
-  });
+  });*/
 });
 
 http.listen(4000);
--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,16 +1,33 @@
 void setup() {
   Serial.begin(9600);
-  pinMode(3, OUTPUT);
-  analogWrite(3, 0);
 }
   
 void loop() {
-  if (Serial.available() > 0) {
+  if (Serial.available() > 1) {
+    int command = Serial.read();
     int value = Serial.read();
-    Serial.println(value);
-    if (value >= 0 && value <= 255) {
-
-      analogWrite(3, value);
+    if (command == 0) {
+      configure(command, value);
+    } else if (command == 1) {
+      wait(value);
+    } else if (command <= 13) {
+      controlSpecific(command, value);
     }
+  }
+}
+
+void configure(int pin, int value) {
+  pinMode(pin, value == 0 ? INPUT : OUTPUT);
+}
+
+void wait(int value) {
+  delay(value * 5);
+}
+
+void controlSpecific(int pin, int value) {
+  if (value == 0 || value == 255) {
+    digitalWrite(pin, value == 255 ? HIGH : LOW);
+  } else {
+    analogWrite(pin, value);
   }
 }
